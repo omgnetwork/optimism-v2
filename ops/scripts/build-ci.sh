3764fd6cfab1d01fd83273b09a9d7e1546a6054e
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2022-03-01 14:35:53 +0100

    build and test everything

--- ops/scripts/build-ci.sh
+++ ops/scripts/build-ci.sh
@@ -1,11 +1,11 @@
-# yarn
-# yarn build
-docker-compose build --parallel -- l2geth
-# docker-compose build --parallel -- builder l2geth l1_chain
-# docker-compose build --parallel -- deployer dtl batch_submitter relayer integration_tests
-# docker-compose build --parallel -- boba_message-relayer-fast gas_oracle boba_deployer
-# docker-compose build --parallel -- fraud-detector monitor
+yarn
+yarn build
 
-# docker ps
+docker-compose build --parallel -- builder l2geth l1_chain
+docker-compose build --parallel -- deployer dtl batch_submitter relayer integration_tests
+docker-compose build --parallel -- boba_message-relayer-fast gas_oracle boba_deployer
+docker-compose build --parallel -- fraud-detector monitor
 
-# wait
+docker ps
+
+wait
