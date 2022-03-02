6a18d5bcceee0fd31d4a2d0247414c79a6c8c7b8
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2022-03-02 11:46:30 +0100

    increase timeout in docker http calls, remove gas oracle and add it into side compose

--- ops/scripts/build-ci.sh
+++ ops/scripts/build-ci.sh
@@ -3,7 +3,7 @@ yarn build
 
 docker-compose build --parallel -- builder l2geth l1_chain
 docker-compose build --parallel -- deployer dtl batch_submitter relayer integration_tests
-docker-compose build --parallel -- boba_message-relayer-fast gas_oracle boba_deployer fraud-detector
+docker-compose build --parallel -- boba_message-relayer-fast boba_deployer fraud-detector
 
 docker ps
 
--- ops/scripts/integration-tests.sh
+++ ops/scripts/integration-tests.sh
@@ -3,19 +3,19 @@ set -e
 
 RETRIES=${RETRIES:-120}
 JSON='{"jsonrpc":"2.0","id":0,"method":"rollup_getInfo","params":[]}'
-
+echo "Calling: "$URL
 if [[ ! -z "$URL" ]]; then
     # get the addrs from the URL provided
     ADDRESSES=$(curl --fail --show-error --silent --retry-connrefused --retry $RETRIES --retry-delay 5 $URL)
     export ADDRESS_MANAGER=$(echo $ADDRESSES | jq -r '.AddressManager')
 fi
-
+echo "Calling: "$BOBA_URL
 if [[ ! -z "$BOBA_URL" ]]; then
     # get the addrs from the URL provided
     ADDRESSES=$(curl --fail --show-error --silent --retry-connrefused --retry $RETRIES --retry-delay 5 $BOBA_URL)
     echo $ADDRESSES | jq -r '.L2LiquidityPool'
 fi
-
+echo "Calling: "$L2_URL
 # wait for the sequencer to be up
 curl \
     --silent \
